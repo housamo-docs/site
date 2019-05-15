8b9260a7b87d86441851c130c9b3b6ba19f34929
HeskeyBaozi <hezhiyu233@foxmail.com>
2019-05-15 21:57:49 +0800

    lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,22 +2,53 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: `Title from siteMetadata 233`
+    title: 'Title from siteMetadata 233'
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `src`,
+        name: 'src',
         path: path.resolve(__dirname, './src')
       }
     },
-    `gatsby-plugin-typescript`,
+    'gatsby-plugin-typescript',
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `src/utils/typography`
+        pathToConfigModule: 'src/utils/typography'
       }
     }
+    // {
+    //   // Querying to a SQLite database
+    //   resolve: `gatsby-source-sql`,
+    //   options: {
+    //     typeName: 'Nirvana',
+    //     // This is the field under which the data will be accessible in a future version
+    //     fieldName: 'chinook',
+    //     dbEngine: {
+    //       client: 'sqlite3',
+    //       connection: {
+    //         filename: './data/Chinook_Sqlite.sqlite'
+    //       },
+    //       useNullAsDefault: true
+    //     },
+    //     queryChain: function(x) {
+    //       return x
+    //         .select(
+    //           'Track.TrackId as TrackId',
+    //           'Track.Name as Track',
+    //           'Album.Title as Album',
+    //           'Genre.Name as Genre',
+    //           'Artist.Name as Artist'
+    //         )
+    //         .from('Track')
+    //         .innerJoin('Album', 'Album.AlbumId', 'Track.AlbumId')
+    //         .innerJoin('Artist', 'Artist.ArtistId', 'Album.ArtistId')
+    //         .innerJoin('Genre', 'Genre.GenreId', 'Track.GenreId')
+    //         .where('Artist.Name', '=', 'Nirvana');
+    //     }
+    //   }
+    // }
   ]
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
 import Container from '../components/container';
+import Button from '@material-ui/core/Button';
 
 export const query = graphql`
   query Test {
@@ -12,16 +13,23 @@ export const query = graphql`
   }
 `;
 
-export default (({ data }) => (
-  <Container>
-    <h1>[Testing] Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>About {data.site.siteMetadata.title}</p>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
-    <pre>
-      <code>Hello World</code>
-    </pre>
-  </Container>
-)) as React.SFC<any>;
+const Index: React.SFC<{ data: any }> = ({ data }) => {
+  return (
+    <Container>
+      <p>About {data.site.siteMetadata.title}</p>
+      <p>
+        What do I like to do?Lots of course but definitely enjoy building
+        {`a${data}`}
+        websites.
+      </p>
+      <pre>
+        <code>Hello World</code>
+      </pre>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </Container>
+  );
+};
+
+export default Index;
